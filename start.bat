4dd7825be7cabbcf5402232a056fae937fec20f4
bandjeo <branislav.andjelic@gmail.com>
2021-05-23 18:31:19 +0200

    Add batch script for installing plugins

---
+++ start.bat
@@ -0,0 +1,13 @@
+cd Core
+start python setup.py install
+cd ..
+cd ExpressGenerator
+start python setup.py install
+cd ..
+cd ReactGenerator
+start python setup.py install
+cd ..
+cd SQLiteGenerator
+start python setup.py install
+
+cd ..
